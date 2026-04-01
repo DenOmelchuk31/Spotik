407dcad34bff433f8deb7a7a0c34016a39193ebb
Vladosik <vg6860299@gmail.com>
2026-04-02 00:10:30 +0300

    finish Track num1

--- Track.cpp
+++ Track.cpp
@@ -3,18 +3,31 @@
 Track::Track()
     : title(""),
     artist(""),
+    album(""),
+    genre(""),
     duration(0),
     filePath(""),
-    isFavorite(false)
+    isFavorite(false),
+    playCount(0)
 {
 }
 
-Track::Track(const QString& title, const QString& artist, int duration, const QString& filePath, bool isFavorite)
+Track::Track(const QString& title,
+             const QString& artist,
+             const QString& album,
+             const QString& genre,
+             int duration,
+             const QString& filePath,
+             bool isFavorite,
+             int playCount)
     : title(title),
     artist(artist),
+    album(album),
+    genre(genre),
     duration(duration),
     filePath(filePath),
-    isFavorite(isFavorite)
+    isFavorite(isFavorite),
+    playCount(playCount)
 {
 }
 
@@ -26,6 +39,16 @@ QString Track::getTitle() const
 QString Track::getArtist() const
 {
     return artist;
+}
+
+QString Track::getAlbum() const
+{
+    return album;
+}
+
+QString Track::getGenre() const
+{
+    return genre;
 }
 
 int Track::getDuration() const
@@ -43,6 +66,11 @@ bool Track::getFavorite() const
     return isFavorite;
 }
 
+int Track::getPlayCount() const
+{
+    return playCount;
+}
+
 void Track::setTitle(const QString& title)
 {
     this->title = title;
@@ -53,9 +81,22 @@ void Track::setArtist(const QString& artist)
     this->artist = artist;
 }
 
+void Track::setAlbum(const QString& album)
+{
+    this->album = album;
+}
+
+void Track::setGenre(const QString& genre)
+{
+    this->genre = genre;
+}
+
 void Track::setDuration(int duration)
 {
-    this->duration = duration;
+    if (duration >= 0)
+    {
+        this->duration = duration;
+    }
 }
 
 void Track::setFilePath(const QString& filePath)
@@ -66,4 +107,43 @@ void Track::setFilePath(const QString& filePath)
 void Track::setFavorite(bool value)
 {
     isFavorite = value;
+}
+
+void Track::setPlayCount(int count)
+{
+    if (count >= 0)
+    {
+        playCount = count;
+    }
+}
+
+void Track::incrementPlayCount()
+{
+    playCount++;
+}
+
+bool Track::isValid() const
+{
+    return !title.trimmed().isEmpty()
+    && !artist.trimmed().isEmpty()
+        && duration > 0
+        && !filePath.trimmed().isEmpty();
+}
+
+QString Track::getFormattedDuration() const
+{
+    int minutes = duration / 60;
+    int seconds = duration % 60;
+
+    QString secondsText;
+    if (seconds < 10)
+    {
+        secondsText = "0" + QString::number(seconds);
+    }
+    else
+    {
+        secondsText = QString::number(seconds);
+    }
+
+    return QString::number(minutes) + ":" + secondsText;
 }
--- Track.h
+++ Track.h
@@ -7,23 +7,44 @@ class Track
 private:
     QString title;
     QString artist;
+    QString album;
+    QString genre;
     int duration;
     QString filePath;
     bool isFavorite;
+    int playCount;
 
 public:
     Track();
-    Track(const QString& title, const QString& artist, int duration, const QString& filePath, bool isFavorite = false);
+    Track(const QString& title,
+          const QString& artist,
+          const QString& album,
+          const QString& genre,
+          int duration,
+          const QString& filePath,
+          bool isFavorite = false,
+          int playCount = 0);
 
     QString getTitle() const;
     QString getArtist() const;
+    QString getAlbum() const;
+    QString getGenre() const;
     int getDuration() const;
     QString getFilePath() const;
     bool getFavorite() const;
+    int getPlayCount() const;
 
     void setTitle(const QString& title);
     void setArtist(const QString& artist);
+    void setAlbum(const QString& album);
+    void setGenre(const QString& genre);
     void setDuration(int duration);
     void setFilePath(const QString& filePath);
     void setFavorite(bool value);
+    void setPlayCount(int count);
+
+    void incrementPlayCount();
+
+    bool isValid() const;
+    QString getFormattedDuration() const;
 };
