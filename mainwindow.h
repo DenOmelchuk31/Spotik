4ac2b2f07717bf1d4c677cfde9931299ae151af5
Den Omelchuk <omelchuk.den2025@gmail.com>
2026-04-02 17:21:20 +0300

    Design improve

--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMediaPlayer>
+#include <QAudioOutput>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +17,19 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void onDownloadBtnClicked();
+    void onPlayBtnClicked();
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer   *m_player;
+    QAudioOutput   *m_audio;
+
+    void updateNowPlaying(const QString &filePath);
 };
-#endif // MAINWINDOW_H
+
+#endif
